efc73f0b27e910b371e160c109c6308e84142420
ayoubbarnaoui35@gmail.com <ayoubbarnaoui35@gmail.com>
2023-01-08 19:27:38 +0100

    commit test fonctionnel

--- src/test/java/com/devrezaur/main/SpringBootQuizAppApplicationTests.java
+++ src/test/java/com/devrezaur/main/SpringBootQuizAppApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootQuizAppApplicationTests {
-
+//	@Autowired
+//	private TestPerformance testP;
 	@Test
 	void contextLoads() {
+//		testP.MySeleniumTest();
 	}
 
 }
